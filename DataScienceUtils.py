3261a3cea4de5565158dd09f7e3a4e9fe88816b3
Kodiak Labs <kodiak.labs@fastmail.fm>
2018-09-03 11:38:38 +0200

    Added grid search version of hyperparameter search

--- DataScienceUtils.py
+++ DataScienceUtils.py
@@ -56,6 +56,36 @@ def random_tuner(X, y, pipline, hyper_params, n_iter_search,
     return model
 
 
+def grid_tuner(X, y, pipline, hyper_params, scoring_metric):
+    """
+    Grid search version of random_tuner. For parameter description, see
+    documentation of random_tuner.
+
+    NB: It is recommended that one uses random search of the hyper parameter
+    space, as that is emperically more efficient [1].
+
+    [1] http://jmlr.org/papers/volume13/bergstra12a/bergstra12a.pdf
+    """
+    model = GridSearchCV(pipeline, parameters, n_jobs=-1, verbose=1,
+                         scoring=scoring_metric)
+
+    start_t = time.time()
+    print('\nTraining multi-output model...')
+
+    model.fit(train_x, train_y)
+
+    del_t = time.time() - start_t
+    print('Time taken (secs): ', del_t)
+
+    print("Best score: %0.3f" % model.best_score_)
+    print("Best parameters set:")
+    best_parameters = model.best_estimator_.get_params()
+    for param_name in sorted(parameters.keys()):
+        print("\t%s: %r" % (param_name, best_parameters[param_name]))
+
+    return model
+
+
 # Classifiers
 def logreg_hype():
     hyper_params = {'penalty': ['l1', 'l2'],
